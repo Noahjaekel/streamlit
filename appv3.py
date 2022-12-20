632bdf233a05b1984a10f2f16b8e134543d0aac8
Noahjaekel <116066208+Noahjaekel@users.noreply.github.com>
2022-12-20 21:51:40 +0100

    Update appv3.py

--- appv3.py
+++ appv3.py
@@ -231,7 +231,7 @@ with Exploratives:
     color_discrete_map=c)
     fig_GarageCars.update_layout(showlegend=False)
     
- with Exploratives:
+  with Exploratives:
     plot1, plot2, plot3 = st.columns(3)
     plot1.subheader("Distribution of Overall Quality")
     plot1.plotly_chart(fig_OverallQual, use_container_width=True)
@@ -250,5 +250,3 @@ with House_Price:
     predictions = pd.concat([predictions, append])
     fig = px.bar(predictions, x="", y="potential", color="improvement", title="Improvement Potential")
     st.plotly_chart(fig, use_container_width=True)
-        
-        

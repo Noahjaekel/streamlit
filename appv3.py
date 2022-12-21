0d6b003ae8f2c700decc65dfbe0628cc7fbcd333
nheim7 <117458389+nheim7@users.noreply.github.com>
2022-12-21 10:13:29 +0100

    Update appv3.py

--- appv3.py
+++ appv3.py
@@ -284,13 +284,7 @@ with Capstone_Project:
 
   
   st.subheader("Training our Model")
-  st.write("__We used the following machine learning algorithms__:")
-  
-  data = [['Column 1', 'Column 2']]
-        
-
-# Create the table and disable the display of row numbers
-  st.table(data, index=False)
+  st.write("We used the following machine learning algorithms")      
   
   data = [["RandomForest", "RandomForest with Hypertuning", "RandomForest with randomizedgridsearch and Hypertuning", "LinearRegression", "DecisionTree", "LassoModel"]]
   

7af9703933af3d6f5dafaff7514a316b36b57983
nheim7 <117458389+nheim7@users.noreply.github.com>
2022-12-21 09:00:42 +0100

    Update appv3.py

--- appv3.py
+++ appv3.py
@@ -283,9 +283,8 @@ with Capstone_Project:
   st.markdown("1. Drop all columns with >90% Na Values. => 2. KNN Imputation for numerical values => 3. Mode Imputation for categorical values.")
 
   
-  st.subheader("We used the following methods:")
-  data = [
-    ["RandomForest", "RandomForest with Hypertuning", "RandomForest with randomizedgridsearch and Hypertuning", "LinearRegression", "DecisionTree", "LassoModel"]]
+  st.subheader("We used the following machine learning algorithms:")
+  data = [["RandomForest", "RandomForest with Hypertuning", "RandomForest with randomizedgridsearch and Hypertuning", "LinearRegression", "DecisionTree", "LassoModel"]]
   
   df = pd.DataFrame(data)
 

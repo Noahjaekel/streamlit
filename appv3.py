d4bc80a6f32449a4bf710d5d82ee447c547ae1af
Noahjaekel <116066208+Noahjaekel@users.noreply.github.com>
2022-12-21 10:32:19 +0100

    Update appv3.py

--- appv3.py
+++ appv3.py
@@ -615,7 +615,7 @@ with Capstone_Project:
     [0.05, "GarageCars", "Size of garage in car capacity"],
     [0.04, "BsmtFinSF1", "Type 1 finished square feet"],
     [0.03, "TotalBsmtSF", "Total square feet of basement area"]
-  df = pd.DataFrame(data, columns=["feature importance", "variable", "meaning of the variable"])
+  df = pd.DataFrame(data, columns=["feature_importance", "variable", "meaning_of_the_variable"])
 
   st.dataframe(df)
   
